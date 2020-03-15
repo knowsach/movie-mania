bed87ff04972ad54b23b9c35160f1a6f2c97efe9
knowsach <knowsach@gmail.com>
2020-03-15 13:23:04 +0530

    environment variable added for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://api.themoviedb.org/3',
 };
+
+export const Api = {
+  api_key: 'caaff7ac5789b759861d8425f37186e7'
+}
